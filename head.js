416c3b1aea3fbf3958f55d112610ee5d4651db4b
Anam Shaikh <anam.shk@outlook.com>
2021-10-25 22:41:59 -0400

    implement to head

--- head.js
+++ head.js
@@ -1,11 +1,18 @@
+const head = function (array) {
+    if (array.length === 0) {
+        return undefined
+    } else {
+        return array[0];
+    }
+  };
+
 const assertEqual = function(actual, expected) {
     if (actual === expected) {
-      console.log(`🙂 , Assertion Passed:, ${actual}, ===, ${expected}`);
+      console.log(`🙂 Assertion Passed: ${actual} === ${expected}`);
     } else {
-      console.log(`😔 , Assertion Failed:, ${actual}, !==, ${expected}`);
+      console.log(`😔 Assertion Failed: ${actual} !== ${expected}`);
     }
-  
   };
   
-  assertEqual("Lighthouse Labs", "Bootcamp");
-  assertEqual(1, 1);
+  assertEqual(head([5,6,7]), 7);
+  assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");

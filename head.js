fbe6eb8f61a9f3eaa422b1bb81c7bb64e46709cd
Anam Shaikh <anam.shk@outlook.com>
2021-10-25 22:48:38 -0400

    enter

--- head.js
+++ head.js
@@ -1,18 +1,18 @@
-const head = function (array) {
-    if (array.length === 0) {
-        return undefined
-    } else {
-        return array[0];
-    }
-  };
+const head = function(array) {
+  if (array.length === 0) {
+    return undefined;
+  } else {
+    return array[0];
+  }
+};
 
 const assertEqual = function(actual, expected) {
-    if (actual === expected) {
-      console.log(`🙂 Assertion Passed: ${actual} === ${expected}`);
-    } else {
-      console.log(`😔 Assertion Failed: ${actual} !== ${expected}`);
-    }
-  };
+  if (actual === expected) {
+    console.log(`🙂 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`😔 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
   
-  assertEqual(head([5,6,7]), 7);
-  assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([5,6,7]), 7);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");

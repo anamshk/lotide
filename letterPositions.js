451fc9732c170d1c9106e3e6a84f17b38fadb99c
Anam Shaikh <anam.shk@outlook.com>
2021-10-27 22:34:32 -0400

    commit to master

---
+++ letterPositions.js
@@ -0,0 +1,37 @@
+const letterPositions = function(sentence) {
+  let result =  {};
+  for (let i = 0; i < sentence.length; i++) {
+    if (result[sentence[i]]) {
+      result[sentence[i]].push(i);
+    } else {
+      result[sentence[i]] = [i];
+    }
+  }
+  return result;
+};
+	
+let helloPositions = letterPositions("hello");
+console.log(helloPositions);
+
+
+const eqArrays = function(left, right) {
+  if (left.length !== right.length) {
+    return false;
+  }
+  for (let i = 0; i < left.length; i++) {
+    if (left[i] !== right[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (!eqArrays(actual, expected)) {
+    console.log(`😔 , Assertion Failed: ${actual}, !==, ${expected}`);
+  } else {
+    console.log(`🙂 , Assertion Passed: ${actual}, ===, ${expected}`);
+  }
+
+};
+assertArraysEqual(letterPositions("hello").l, [2,3]);

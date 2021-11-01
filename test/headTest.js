3007bc11b5b2f1de06afeaa23d7a8cc5b1dbed35
Anam Shaikh <anam.shk@outlook.com>
2021-11-01 17:12:22 -0400

    changes with mocha chai inclusion

--- test/headTest.js
+++ test/headTest.js
@@ -1,7 +1,20 @@
-const assertEqual = require('../assertEqual');
+// const assertEqual = require('../assertEqual');
 
-// test/head.js
-const head = require('../head');
+// // test/head.js
+// const head = require('../head');
 
-assertEqual(head([5,6,7]), 7);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+// assertEqual(head([5,6,7]), 7);
+// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+const assert = require('chai').assert;
+const head   = require('../head');
+
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+});

3d672a4dff87166f1c7c00a68d4f5d2660833803
Anam Shaikh <anam.shk@outlook.com>
2021-11-01 16:21:59 -0400

    added test property

---
+++ test/eqArrayTest.js
@@ -0,0 +1,8 @@
+// test/assertEqualTest.js
+const assertEqual = require('../assertEqual');
+
+//test/eqArrays.js
+
+const eqArrays = require('../eqArrays');
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
---
+++ test/headTest.js
@@ -0,0 +1,7 @@
+const assertEqual = require('../assertEqual');
+
+// test/head.js
+const head = require('../head');
+
+assertEqual(head([5,6,7]), 7);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
